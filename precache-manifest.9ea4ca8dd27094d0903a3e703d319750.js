1ba1ecfc31249a1ac59e5c9f36daadea5eca43ce
kurosakikulot <50890128+kurosakikulot@users.noreply.github.com>
2020-02-29 14:28:06 +0800

    Updates

---
+++ precache-manifest.9ea4ca8dd27094d0903a3e703d319750.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cd9e32e134ea91f347217a41be25c2",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "e1783b35952e5f1e2e0a",
+    "url": "/RecipeApp/static/js/2.0d88ea7c.chunk.js"
+  },
+  {
+    "revision": "a781faf69a68775aa27e",
+    "url": "/RecipeApp/static/js/main.2c440efe.chunk.js"
+  },
+  {
+    "revision": "abfae573c7f8cdbc5d0c",
+    "url": "/RecipeApp/static/js/runtime-main.0a02413c.js"
+  }
+]);

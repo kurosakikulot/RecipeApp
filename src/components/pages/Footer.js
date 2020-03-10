e7e08a1fc1d466c216bf4f6f1e2a55348f6f2b1b
kurosakikulot <50890128+kurosakikulot@users.noreply.github.com>
2020-03-11 03:13:58 +0800

    change for Footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -2,26 +2,28 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="page-footer black footer">
+    <footer className="page-footer black">
       <div className="container">
         <div className="row">
-          <div className="footer-copyright col s4">
-            <h6>© 2020 Copyright Reserved</h6>
+          <div className="col l6 s6">
+            <h5 className="white-text">Follow Me</h5>
+            <p className="grey-text text-lighten-4">
+              <a
+                className="footer-icon"
+                href="https://www.facebook.com/Kurosakikulot"
+              >
+                <i className="fab fa-facebook-square small"></i>
+              </a>
+              <a
+                className="footer-icon"
+                href="https://github.com/kurosakikulot"
+              >
+                <i className="fab fa-github small"></i>
+              </a>
+            </p>
           </div>
-          <div className="col s4">
-            <h6> Follow Me </h6>
-            <a
-              className="footer-icon"
-              href="https://www.facebook.com/Kurosakikulot"
-            >
-              <i className="fab fa-facebook-square small"></i>
-            </a>
-            <a className="footer-icon" href="https://github.com/kurosakikulot">
-              <i className="fab fa-github small"></i>
-            </a>{" "}
-          </div>
-          <div className="col s4">
-            <h6 className="white-text">Powered by: </h6>
+          <div className="col l6 s6">
+            <h5 className="white-text">Powered by</h5>
             <a
               className="grey-text text-lighten-4"
               href="https://www.edamam.com/"
@@ -31,6 +33,9 @@ export default function Footer() {
             </a>
           </div>
         </div>
+      </div>
+      <div className="footer-copyright">
+        <div className="container">© 2020 Copyright Reserved</div>
       </div>
     </footer>
   );
